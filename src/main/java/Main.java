1d4075401f7dedd71a6c2d50391b446dd6019915
EvgeniiaSanochkina <sanochkina.es2501@gmail.com>
2022-08-14 00:43:34 +0700

    done

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        int price = 10_000;
+        int miles = (int) service.calculate(price, true);
+        System.out.println(miles);
+    }
+}
